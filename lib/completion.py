962875dfb1dbd048b6b97cec184954d4e357db0e
Dmitry Sadovnychyi <git@dmit.ro>
2015-10-05 16:32:35 +0800

    Remove Jedi from completions

--- lib/completion.py
+++ lib/completion.py
@@ -5,6 +5,8 @@ import json
 import traceback
 sys.path.append(os.path.dirname(__file__))
 import jedi
+# remove jedi from path after we import it so it will not be completed
+sys.path.pop(0)
 
 unicode = str if 'unicode' not in dir(__builtins__) else unicode
 MAX_LENGTH = 70
